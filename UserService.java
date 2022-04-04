bb0fafe0a6b1487b2276ad3f7a2fccf545fd0a89
tristandanle <letristandan@gmail.com>
2022-04-04 06:00:34 -0400

    Update UserService.java

--- UserService.java
+++ UserService.java
@@ -25,52 +25,50 @@ public class UserService {
 		System.out.println("Enter your email: ");
 		System.out.println(" ");
 		String username = null;
-		boolean valide = true;
-		 do {
-			
-			username = scanner().nextLine();
-			if (!(username.equals(username2))){
-				
-				System.out.println(" ");
-				System.out.println("NO MATCH");
-				System.out.println(" ");
-				System.out.println("Enter your email: ");
-				System.out.println(" ");
-			} else {
-				valide = false;
-			}
-					}
-		while(valide == true);
+		// boolean valide = true;
+		// do {
+
+		username = scanner().nextLine();
+		// if (!(username.equalsIgnoreCase(username2))){
+
+		System.out.println(" ");
+		// System.out.println("NO MATCH");
+		// System.out.println(" ");
+		// System.out.println("Enter your email: ");
+		// System.out.println(" ");
+		// } else {
+		// valide = false;
+		// }
+		// }
+		// while(valide == true);
 		return username;
 	}// End method inputUsername()
- 
 
 	public String inputPassword(String password2) {
-		System.out.println();
+		// System.out.println();
 		System.out.println("Enter your password: ");
 		System.out.println(" ");
 		String password = null;
-		boolean valide = true;
-		 do {
-			password = scanner().nextLine();
-			if (!(password.equals(password2))){
-				
-				System.out.println(" ");
-				System.out.println("NO MATCH");
-				System.out.println(" ");
-				System.out.println("Enter your password: ");
-				System.out.println(" ");
-			} else {
-				valide = false;
-			}
-					}
-		while(valide == true);
+		// boolean valide = true;
+		// do {
+		password = scanner().nextLine();
+		// if (!(password.equals(password2))){
+		System.out.println(" ");
+		// System.out.println("NO MATCH");
+		// System.out.println(" ");
+		// System.out.println("Enter your password: ");
+		// System.out.println(" ");
+		// } else {
+		// valide = false;
+		// }
+		// }
+		// while(valide == true);
 		return password;
 	}// End method inputPassword()
 
 	public boolean validateUser(User user) {
 		int MAX_ATTEMPT = 5;
-		String name;		
+		String name;
 		boolean flag = false;
 		boolean valid = false;
 		String password;
@@ -79,14 +77,14 @@ public class UserService {
 		for (int i = 0; i < MAX_ATTEMPT; i++) {
 			username = inputUsername(user.getUserName());
 			password = inputPassword(user.getPassword());
-			if ((user.getUserName().equals(username)) && (user.getPassword().equals(password))) {
+			if ((user.getUserName().equalsIgnoreCase(username)) && (user.getPassword().equals(password))) {
 				flag = true;
-				//valid = true;
+				// valid = true;
 				break;
 			} else {
-				System.out.println();
-				if(i < (MAX_ATTEMPT - 1)) {
-				System.out.println("Invalid login, please try again");
+				// System.out.println();
+				if (i < (MAX_ATTEMPT - 1)) {
+					System.out.println("Invalid login, please try again");
 				}
 			}
 		} // End for loop
