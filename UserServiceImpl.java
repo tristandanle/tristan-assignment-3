1dda9c0cbf1918825d33302c7e6eeeed94b36052
tristandanle <letristandan@gmail.com>
2022-04-10 18:52:38 -0400

    Update UserServiceImpl.java

--- UserServiceImpl.java
+++ UserServiceImpl.java
@@ -13,7 +13,7 @@ public class UserServiceImpl implements UserService {
 		UserService userService = new UserServiceImpl();
 		String inUser;
 		String inPassword;
-		
+
 		User[] users = new User[4];
 		int i = 0;
 		try {
@@ -22,20 +22,19 @@ public class UserServiceImpl implements UserService {
 
 			String line;
 			while ((line = fileReader.readLine()) != null) {
-				//users[i] = userService.createUser(line);
-				//System.out.println(line);
+				// users[i] = userService.createUser(line);
+				// System.out.println(line);
 				String[] stringArray = line.split(",");
 				User user = new User();
 				user.setUserName(stringArray[0]);
 				user.setPassword(stringArray[1]);
 				user.setName(stringArray[2]);
 				users[i] = user;
-				i ++;
-				//System.out.println(user.getUserName() + user.getPassword() + user.getName());
+				i++;
+				// System.out.println(user.getUserName() + user.getPassword() + user.getName());
 
 			}
-			
-			
+
 		} catch (FileNotFoundException e) {
 			System.out.println("The file wasn't found ");
 			e.printStackTrace();
@@ -52,62 +51,9 @@ public class UserServiceImpl implements UserService {
 			}
 
 		}
-		
+
 		return users;
 	}
 
-	@Override
-	public String inputUsername() {
-		System.out.println("Enter your email: ");
-		System.out.println(" ");
-		String username = null;
-		Scanner scan = new Scanner(System.in);
-		username = scan.nextLine();
-
-		return username;
-
-	}
-
-	@Override
-	public String inputPassword() {
-		// System.out.println();
-		System.out.println("Enter your password: ");
-		System.out.println(" ");
-		String password = null;
-		Scanner scan = new Scanner(System.in);
-		password = scan.nextLine();
-
-		return password;
-	}
-
-	@Override
-	public boolean validateUser(User user) {
-		int MAX_ATTEMPT = 5;
-		// String name;
-		boolean isValid = true;
-		// boolean valid = false;
-		String password ;
-		String username ;
-
-		//for (int i = 0; i < MAX_ATTEMPT; i++) {
-    
-			System.out.println("Invalid login, please try again");
-			username = inputUsername();
-			password = inputPassword();
-            
-			if ( ((user.getUserName().equalsIgnoreCase(username)) 
-					&& (user.getPassword().equals(password))) ) {
-				isValid = true;
-			
-            } else {
-            	isValid = false;
-            }
-//			if (i == MAX_ATTEMPT - 1) {
-//				isValid = false;
-//		    }
-		
-		
-		// break;
-        return isValid;
-	}
 }
+
