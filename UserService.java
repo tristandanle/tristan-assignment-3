7c3e5d9d98355b9034b7ab64ea0c8ea7c3aab0af
tristandanle <letristandan@gmail.com>
2022-04-10 18:51:23 -0400

    Update UserService.java

--- UserService.java
+++ UserService.java
@@ -4,10 +4,5 @@ import java.util.Scanner;
 
 public interface UserService {
 	public User[] loadUser(String fileName);
-	public String inputUsername();	
-	public String inputPassword();
-	//public boolean validateUser(User[] user, String inUser, String inPassword);
-	public boolean validateUser(User user);
-
 }	
 	
