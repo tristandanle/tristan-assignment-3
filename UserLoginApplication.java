c907af51a8169c45ebbd2104cd9ea87923ed8b40
tristandanle <letristandan@gmail.com>
2022-04-17 19:41:37 -0400

    Update UserLoginApplication.java
    Latest version - UserLoginApplication.java  - 2022-04-17
    Goals:
    ****** 
    - Mimic a user login from a Java console application
    - Prompt a user for asking a username and a password as inputs.  
    - Validate the combination of these inputs with  the username and password in the data.txt file if they are matched.
    - If information are matched, display a Welcome message with that user. If not, display the invalid message and ask to try again.

--- UserLoginApplication.java
+++ UserLoginApplication.java
@@ -1,5 +1,7 @@
 package com.coderscampus.assignment3;
 
+package com.coderscampus.userloginapp;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -15,49 +17,49 @@ public class UserLoginApplication {
 		int i = 0;
 		User[] users = new User[SIZE];
 		BufferedReader br = new BufferedReader(new FileReader("data.txt"));
-		Scanner scan = new Scanner(System.in);
-		
+		Scanner scanner = new Scanner(System.in);
+
 		// Read users from file
 		loadUsers(i, users, br);
 
-		
 		while (num < MAX_Attempts) {
+			boolean isValid = true;
 			// Scan infos from keyboard
 			System.out.println("Enter your email: ");
 			System.out.println();
-			String inpUser = scan.nextLine();
+			String inpUser = scanner.nextLine();
+			System.out.println();
 			System.out.println("Enter your password: ");
 			System.out.println();
-			String inpPass = scan.nextLine();
-			
+			String inpPass = scanner.nextLine();
+
 			for (User user : users) {
-//				System.out.println(user.getUserName());
-//				System.out.println(user.getPassword());
 				if (!(user.getUserName().equalsIgnoreCase(inpUser) && user.getPassword().equals(inpPass))) {
-					//System.out.println("Invalid login, please try again");
+					isValid = false;
 					continue;
-				} 				
-					System.out.println("Welcome: " + user.getName());
-					System.exit(1);
-				
+				}
+				System.out.println();
+				System.out.println("Welcome: " + user.getName());
+				System.exit(1);
+
 			}
 
 			num++;
-			//System.out.println(num);
-			}
-			if (num == MAX_Attempts ) {
+			if (isValid == false) {
 				System.out.println();
-				System.out.println("Too many failed login attempts, you are now locked out. ");
+				System.out.println("Invalid login, please try again");
+				System.out.println();
 			}
-	     
-	    }// end main
-		
+		}
+		if (num == MAX_Attempts) {
+			System.out.println("Too many failed login attempts, you are now locked out. ");
+		}
 
-	
+	}// end main
 
 	public static void loadUsers(int i, User[] users, BufferedReader br) throws IOException {
-		String line=null;
-		String[] lineData= null;
+		String line = null;
+		String[] lineData = null;
 		while ((line = br.readLine()) != null) {
 			lineData = line.split(",");
 			User user = new User(lineData[0], lineData[1], lineData[2]);
@@ -66,6 +68,4 @@ public class UserLoginApplication {
 		}
 	}
 
-	
 } // end class
-
